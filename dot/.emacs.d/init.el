395245a8b2f059cf90f5d1e4556e648eb5c3d81a
Gregory Gundersen <ggundersen@gmail.com>
2022-10-29 13:02:31 -0400

    Clean up.

--- dot/.emacs.d/init.el
+++ dot/.emacs.d/init.el
@@ -1,6 +1,6 @@
 ;; =================================================================-*- lisp -*-
 ;;
-;; emacs config.
+;; emacs config
 ;;
 ;; =============================================================================
 
@@ -8,38 +8,59 @@
 ;; Miscellany
 ;; ==========
 
-; No startup screen, menu, toolbar, and scroll bar.
+;; No startup screen, menu, toolbar, and scroll bar.
 (setq inhibit-startup-screen t)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
-; Ctrl-z for undo.
+;; Ctrl-z for undo.
 (global-set-key [(control z)] 'undo)
 (global-unset-key [(control x)(control z)])
+(global-set-key '[(control z)] 'undo)
 
-; Wrap lines.
+;; Wrap lines.
 (setq-default truncate-lines t)
 
-; Add line numbers.
+;; Add line numbers.
 (column-number-mode)
 (global-display-line-numbers-mode t)
 
-; Show whitespace.
+;; Show whitespace.
 (setq-default show-trailing-whitespace t)
 (global-set-key '[(control ?c) (?w) (?w)] 'whitespace-mode)
 (global-set-key '[(control ?c) (?w) (?o)] 'whitespace-toggle-options)
 
-; Simple dark theme.
+;; Disable bell.
+(setq ring-bell-function 'ignore)
+
+;; Simple dark theme.
 (load-theme 'wombat)
+
+;; Auto-complete.
+(global-auto-complete-mode t)
 
 ; Stop modifying init.el with customize.
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (load custom-file 'noerror)
 
-; Place all temp files in ~/.saves.
+;; Place all temp files in ~/.saves.
 (setq backup-directory-alist `(("." . "~/.saves")))
 (setq backup-by-copying t)
+
+
+;; ===============
+;; Package manager
+;; ===============
+
+(require 'package)
+(require 'use-package)
+(add-to-list 'package-archives
+	'("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+(add-to-list 'load-path "~/.emacs.d/elisp")
 
 
 ;; ============
@@ -47,8 +68,8 @@
 ;; ============
 
 ; Full screen on startup.
-;(set-frame-parameter (selected-frame) 'fullscreen 'maximized)
-;(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(set-frame-parameter (selected-frame) 'fullscreen 'maximized)
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
 
 ; Fill text to 80 characters.
 (setq-default fill-column 80)
@@ -64,17 +85,24 @@
 
 
 ;; ===============
-;; Package manager
+;; Vim keybindings
 ;; ===============
 
-(require 'package)
-(require 'use-package)
-(add-to-list 'package-archives
-	'("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives
-	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(package-initialize)
-(add-to-list 'load-path "~/.emacs.d/elisp")
+(require 'evil)
+(evil-mode 1)(require 'evil)
+
+
+;; ============
+;; Project view
+;; ============
+
+;; Display keybinding options of incomplete command.
+(use-package which-key
+  :defer 0
+  :diminish which-key-mode
+  :config
+  (which-key-mode)
+  (setq which-key-idle-delay 0))
 
 
 ;; ==================
@@ -125,8 +153,29 @@
 ;; Functions
 ;; -----------------------------------------------------------------------------
 
+(defun gwg-hline (character)
+  (while (< (current-column) (current-fill-column))
+    (insert character)))
 
+(defun gwg-hline-minus ()
+  (interactive)
+  (insert "# ")
+  (gwg-hline "-"))
 
+(defun gwg-hline-equals ()
+  (interactive)
+  (insert "# ")
+  (gwg-hline "="))
+
+(global-set-key '[(control ?c) (control ?-)] 'gwg-hline-minus)
+(global-set-key '[(control ?c) (control ?=)] 'gwg-hline-equals)
+
+(defun gwg-indent-to-4 ()
+  "Move the point to the next column that is a multiple of 4."
+  (interactive)
+  (indent-to (let ((col (current-column))) (+ col (- 4 (% col 4))))))
+
+(global-set-key '[(meta ?i)] 'gwg-indent-to-4)
 
 
 ;; -----------------------------------------------------------------------------
@@ -140,7 +189,6 @@
 (require 'org)
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done t)
 (setq org-agenda-files   (list "~/org/")
       org-refile-targets '((org-agenda-files :maxlevel . 5))
       org-refile-use-outline-path 'file)
@@ -150,13 +198,14 @@
 ;; Python mode
 ;; ===========
 
+(use-package python-mode
+    :ensure t
+    :hook (python-mode . lsp-deferred))
 
-;; ========
-;; Flycheck
-;; ========
 
-;(use-package flycheck
-;  :config
-;  (setq-default flycheck-disabled-checkers '(python-pylint))
-;  (global-flycheck-mode))
+; Syntax check on-the-fly.
+(use-package flycheck
+  :config
+  (setq-default flycheck-disabled-checkers '(python-pylint))
+  (global-flycheck-mode))
 
